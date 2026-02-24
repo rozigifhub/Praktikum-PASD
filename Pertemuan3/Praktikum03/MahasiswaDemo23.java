39ac9241821792e2d55e8580e390643b0e720d92
rozigifhub <rozigming@gmail.com>
2026-02-24 22:02:11 +0700

    Praktikum 3.3

--- Pertemuan3/Praktikum03/MahasiswaDemo23.java
+++ Pertemuan3/Praktikum03/MahasiswaDemo23.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo23 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa23[] arrayOfMahasiswa = new Mahasiswa23[3];
+        String dummy;
+
+        for (int i = 0; i < 3; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa23();
+
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("--------------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            arrayOfMahasiswa[i].cetakInfo();
+
+        }
+
+        sc.close();
+    }
 }
