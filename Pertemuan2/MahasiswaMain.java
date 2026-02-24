3684dfc241977739fa98732b2d1dd8dcb46951d3
rozigifhub <rozigming@gmail.com>
2026-02-24 11:05:33 +0700

    Laporan pdf

--- Pertemuan2/MahasiswaMain.java
+++ Pertemuan2/MahasiswaMain.java
@@ -2,7 +2,7 @@ public class MahasiswaMain<NoAbsen> {
     public static void main(String[] args) {
         Mahasiswa23 mhs1 = new Mahasiswa23();
         mhs1.nama = "Muhammad Ali Farhan";
-        mhs1.nim  = "2241720171";
+        mhs1.nim = "2241720171";
         mhs1.kelas = "S1 2J";
         mhs1.ipk = 3.55;
 
