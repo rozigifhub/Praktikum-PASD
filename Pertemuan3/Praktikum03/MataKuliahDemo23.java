d0d5fa1963d920d025d597fdb342fc59e0ba85f9
rozigifhub <rozigming@gmail.com>
2026-02-24 22:18:42 +0700

    Praktikum 3.4

---
+++ Pertemuan3/Praktikum03/MataKuliahDemo23.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MataKuliahDemo23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliah23[] arrayOfMatakuliah = new MataKuliah23[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode      : ");
+            kode = sc.nextLine();
+            System.out.print("Nama      : ");
+            nama = sc.nextLine();
+            System.out.print("SKS       : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam: ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("--------------------------------");
+
+            arrayOfMatakuliah[i] = new MataKuliah23(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode      : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama      : " + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks       : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam: " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("------------------------------");
+        }
+
+
+        sc.close();
+    }
+}
